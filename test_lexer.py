223ba299a9cb18ccb4a51a9f06457f235129ca07
Swizec Teller <swizec@swizec.com>
2012-03-31 22:07:40 +0200

    a more useful test case class hierarchy

--- test_lexer.py
+++ test_lexer.py
@@ -1,26 +1,20 @@
 
-import unittest, os, subprocess
+import unittest
 from lxml import etree
 
-class PascalLexerTestCase(unittest.TestCase):
+from test_commons import PascalTestCase
+
+class PascalLexerTestCase(PascalTestCase):
     source = "" # test input file
     lex = etree.ElementTree(etree.Element('xml'))
 
     @classmethod
     def setUpClass(cls):
-        try:
-            os.remove('lexanal.xml')
-        except OSError:
-            pass
+        super(PascalLexerTestCase, cls).setUpClass()
 
-        if cls.source != None:
-            subprocess.call("java -cp pascal/bin/java_cup/runtime:lib/java-cup-11a.jar:pascal/bin/compiler/.. compiler.Main test/"+cls.source.replace(".pascal", "")+" lexanal",
-                            shell=True,
-                            stdout=None)
-
-            parser = etree.XMLParser(recover=True)
-            cls.lex = etree.parse('lexanal.xml', parser)
-            cls.root = cls.lex.getroot()
+        parser = etree.XMLParser(recover=True)
+        cls.lex = etree.parse('lexanal.xml', parser)
+        cls.root = cls.lex.getroot()
 
     def lex_test(self, tag, name=None, token=None, lexeme=None, line=None, column=None):
         if name:
