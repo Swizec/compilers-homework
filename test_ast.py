36d98ae57f681d6cf5846b44722fc1eccac40103
Swizec Teller <swizec@swizec.com>
2012-04-15 00:02:09 +0200

    creating the main node ... I think

--- test_ast.py
+++ test_ast.py
@@ -4,20 +4,20 @@ from test_commons import PascalTestCase
 from lxml import etree
 
 
-class PascalSyntaxTestCase(PascalTestCase):
+class PascalASTTestCase(PascalTestCase):
     stage = "abstree"
     syn = etree.ElementTree(etree.Element('xml'))
 
     @classmethod
     def setUpClass(cls):
-        super(PascalSyntaxTestCase, cls).setUpClass()
+        super(PascalASTTestCase, cls).setUpClass()
 
         parser = etree.XMLParser(recover=True)
         cls.syn = etree.parse('abstree.xml', parser)
         cls.root = cls.syn.getroot()
 
 
-class TestAST(PascalSyntaxTestCase):
+class TestAST(PascalASTTestCase):
     source = "ast.pascal"
 
     def test_syntax(self):
